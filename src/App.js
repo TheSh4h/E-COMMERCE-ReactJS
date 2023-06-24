618af4c21e53f949b4e12f92d7a4604a875d50c4
TheSh4h <hh.haseebshah@gmail.com>
2023-06-24 04:18:57 -0700

    created files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Container, Heading, Text } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Container>
+      <Heading>Chakra UI Components</Heading>
+      <Text>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, voluptatibus.</Text>
+    </Container>
+  );
+}
+
+export default App;
