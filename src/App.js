eeb7cfce2b60a443f34ad2fc24375b7799750d88
TheSh4h <hh.haseebshah@gmail.com>
2023-06-24 05:07:15 -0700

    fixed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
-import { Box, Container, Heading, Text, filter } from "@chakra-ui/react";
 
 function App() {
 
-  const boxStyles = {
-    p: "10px",
-    bg: "purple.400",
-    m: "10px",
-    textAlign: "center",
-    filter: "blur(2px)",
-    ':hover': {
-      color: 'black',
-      bg: 'blue.200'
-    }
-  }
-
   return (
-    <Container as="section" maxWidth="4xl" py="20px">
-      <Heading my="30px" p="10px">Chakra UI Components</Heading>
-      <Text ml="30px">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, voluptatibus.</Text>
-      <Text ml="30px" color="blue.500" fontWeight="bold">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis, voluptatibus.</Text>
-
-      <Box my="30px" p="20px" bg="orange">
-        <Text color="white">This is a box</Text>
-      </Box>
-
-      <Box sx={boxStyles}>
-        Hello, Boyas
-      </Box>
-
-    </Container>
+    <div>
+      <h1>Hello</h1>
+    </div>
   );
 }
 
