a3fc2f1bada7f5f19bd9f38f14d7f6163e3bc85f
TheSh4h <hh.haseebshah@gmail.com>
2023-07-01 10:37:50 -0700

    navbar

---
+++ src/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+    return ( 
+        <div className="flex justify-evenly">
+            <h1>E-Dump</h1>
+            <h1>Categories</h1>
+            <h1>Deals</h1>
+            <h1>What's New</h1>
+            <h1>Delivery</h1>
+
+            <input type="text" placeholder="Search Product" />
+
+            <h1>Account</h1>
+            <h1>Cart</h1>
+        </div>
+     );
+}
+ 
+export default Navbar;
