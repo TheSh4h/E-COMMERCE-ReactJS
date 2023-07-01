a91d633e8c40936a2b033221eb79e98cb2b21669
TheSh4h <hh.haseebshah@gmail.com>
2023-07-01 11:35:05 -0700

    idk

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,16 +1,17 @@
 const Navbar = () => {
     return ( 
-        <div className="flex justify-evenly">
-            <h1>E-Dump</h1>
-            <h1>Categories</h1>
-            <h1>Deals</h1>
-            <h1>What's New</h1>
-            <h1>Delivery</h1>
-
-            <input type="text" placeholder="Search Product" />
-
-            <h1>Account</h1>
-            <h1>Cart</h1>
+        <div className="flex justify-between mr-11 ml-11">
+                <h1>E-Dump</h1>
+                <h1>Categories</h1>
+                <h1>Deals</h1>
+                <h1>What's New</h1>
+                <h1>Delivery</h1>
+        
+                <input type="text" placeholder="Search Product" className="
+                bg-slate-200 rounded-2xl h-7 
+                " />
+                <h1>Account</h1>
+                <h1>Cart</h1>
         </div>
      );
 }
