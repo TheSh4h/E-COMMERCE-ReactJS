f0ca52f1180e8071e2893a148c17b9fbba6d577c
TheSh4h <hh.haseebshah@gmail.com>
2023-07-03 10:53:59 -0700

    idk rounded sum

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -2,15 +2,15 @@ const Gallery = () => {
     return ( 
 
     <div className="grid grid-cols-4 gap-4 m-10 text-center">
-        <div className="bg-slate-400 h-96 w-50"> COLUMN</div>
-        <div className="bg-slate-400 h-96 w-50"> COLUMN</div>
-        <div className="bg-slate-400 h-96 w-50"> COLUMN</div>
-        <div className="grid grid-rows-2 gap-4 w-50"> 
-            <div className="bg-slate-400">
+        <div className="rounded-xl bg-slate-400 h-96 w-50"> COLUMN</div>
+        <div className="rounded-xl bg-slate-400 h-96 w-50"> COLUMN</div>
+        <div className="rounded-xl bg-slate-400 h-96 w-50"> COLUMN</div>
+        <div className="rounded-xl grid grid-rows-2 gap-4 w-50"> 
+            <div className="rounded-xl bg-slate-400 w-50">
                 row
             </div>
 
-            <div className="bg-slate-400">
+            <div className="rounded-xl bg-slate-400 2-50">
                 row
             </div>
         </div>
