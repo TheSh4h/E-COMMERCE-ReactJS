8f4916fef40d386577753f2d1084020ab6c10ad5
TheSh4h <hh.haseebshah@gmail.com>
2023-07-05 09:18:57 -0700

    gallery done

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,44 +1,55 @@
 import books from './img/books.jpg';
-import furn from './img/furn.jpg';
 import furniture from './img/furniture.jpg';
 import gym from './img/gym.jpg';
+import console from './img/console.jpg';
 
 const Gallery = () => {
     return ( 
 
     <div>
 
-        <div className='relative h-96 w-48'>
-            <div>
-                <img src={gym} alt="failed to load" className='object-cover object-left h-96 w-48
-                rounded-2xl'/>
-            </div>
-            <div className='absolute top-2 left-0 right-0 flex items-center justify-center
-            backdrop-filter backdrop-blur-2xl'>
-                <span className='font-semibold text-white'>Gym</span>
-            </div>
-        </div>
+        <div className='flex justify-center grid-cols-4 gap-4 mt-10'>
 
-        <div className='relative h-96 w-48'>
-            <div>
-                <img src={books} alt="failed to load" className='object-none object-center h-96 w-48
-                rounded-2xl'/>
+            <div className='card'>
+                <div>
+                    <img src={gym} alt="failed to load" className='object-cover object-left h-96 w-48
+                    rounded-2xl'/>
+                </div>
+                <div className='badge'>
+                    <span className='font-semibold text-white'>Gym</span>
+                </div>
             </div>
-            <div className='absolute top-2 left-0 right-0 flex items-center justify-center
-            backdrop-filter backdrop-blur-2xl'>
-                <span className='font-semibold text-white'>Books</span>
-            </div>
-        </div>
 
-        <div className='relative h-96 w-48'>
-            <div>
-                <img src={furniture} alt="failed to load" className='object-cover object-right h-96 w-48
-                rounded-2xl'/>
+                <div className='card'>
+                <div>
+                    <img src={books} alt="failed to load" className='object-none object-center h-96 w-48
+                    rounded-2xl'/>
+                </div>
+                <div className='badge'>
+                    <span className='font-semibold text-white'>Books</span>
+                </div>
             </div>
-            <div className='absolute top-2 left-0 right-0 flex items-center justify-center
-            backdrop-filter backdrop-blur-2xl'>
-                <span className='font-semibold text-white'>Furniture</span>
+
+            <div className='card'>
+                <div>
+                    <img src={furniture} alt="failed to load" className='object-cover object-right h-96 w-48
+                    rounded-2xl'/>
+                </div>
+                <div className='badge'>
+                    <span className='font-semibold text-white'>Furniture</span>
+                </div>
             </div>
+
+            <div className='card'>
+                <div>
+                    <img src={console} alt="failed to load" className='object-cover object-center h-96 w-48
+                    rounded-2xl'/>
+                </div>
+                <div className='badge'>
+                    <span className='font-semibold text-white'>Tech</span>
+                </div>
+            </div>
+            
         </div>
 
     </div>
