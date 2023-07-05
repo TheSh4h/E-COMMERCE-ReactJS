9b9ede242d7193dbcf725777003110820dbb6080
TheSh4h <hh.haseebshah@gmail.com>
2023-07-05 08:00:31 -0700

    added stuff

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -1,19 +1,46 @@
+import books from './img/books.jpg';
+import furn from './img/furn.jpg';
+import furniture from './img/furniture.jpg';
+import gym from './img/gym.jpg';
+
 const Gallery = () => {
     return ( 
 
-    <div className="grid grid-cols-4 gap-4 m-10 text-center">
-        <div className="rounded-xl bg-slate-400 h-96 w-50"> COLUMN</div>
-        <div className="rounded-xl bg-slate-400 h-96 w-50"> COLUMN</div>
-        <div className="rounded-xl bg-slate-400 h-96 w-50"> COLUMN</div>
-        <div className="rounded-xl grid grid-rows-2 gap-4 w-50"> 
-            <div className="rounded-xl bg-slate-400 w-50">
-                row
-            </div>
+    <div>
 
-            <div className="rounded-xl bg-slate-400 2-50">
-                row
+        <div className='relative h-96 w-48'>
+            <div>
+                <img src={gym} alt="failed to load" className='object-cover object-left h-96 w-48
+                rounded-2xl'/>
+            </div>
+            <div className='absolute top-2 left-0 right-0 flex items-center justify-center
+            backdrop-filter backdrop-blur-2xl'>
+                <span className='font-semibold text-white'>Gym</span>
             </div>
         </div>
+
+        <div className='relative h-96 w-48'>
+            <div>
+                <img src={books} alt="failed to load" className='object-none object-center h-96 w-48
+                rounded-2xl'/>
+            </div>
+            <div className='absolute top-2 left-0 right-0 flex items-center justify-center
+            backdrop-filter backdrop-blur-2xl'>
+                <span className='font-semibold text-white'>Books</span>
+            </div>
+        </div>
+
+        <div className='relative h-96 w-48'>
+            <div>
+                <img src={furniture} alt="failed to load" className='object-cover object-right h-96 w-48
+                rounded-2xl'/>
+            </div>
+            <div className='absolute top-2 left-0 right-0 flex items-center justify-center
+            backdrop-filter backdrop-blur-2xl'>
+                <span className='font-semibold text-white'>Furniture</span>
+            </div>
+        </div>
+
     </div>
 
     );
