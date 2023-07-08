63640b0b508fe7795915e071c1fe2a51e57344fb
TheSh4h <hh.haseebshah@gmail.com>
2023-07-08 10:49:58 -0700

    cleansed code

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -4,13 +4,13 @@ const ProductList = () => {
     return ( 
         <div className="flex justify-center grid-cols-1 gap-4 p-10">
 
-            <div className="itemCard transition ease-in-out delay-150 hover:translate-y-1 hover:scale-110">
+            <div className="itemCard">
                 <img src={ps5} alt="playstation 5" className='hover:blur-sm transition duration-150'/>
 
                 <div className='text-center'>
                     <span className='font-semibold'>PlayStation 5</span>
                 </div>
-                <div className='absolute top-0 left-2 bg-white rounded-lg p-1 mt-1 text-green-950'>
+                <div className='itemBadge'>
                     <span>In Stock</span>
                 </div>
             </div>
