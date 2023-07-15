7624a95360746e1bec5142dc1b818e8bf66ccd2a
TheSh4h <hh.haseebshah@gmail.com>
2023-07-15 11:15:02 -0700

    searchbar fixed

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -28,9 +28,8 @@ const Navbar = () => {
                 >
                     <h1>Delivery</h1>
                 </div>
-            </div>
 
-            {/* Search field */}
+                {/* Search field */}
             <div className="lg:flex hidden items-center relative">
                 <input type="text" placeholder="Search Product" className="
                 bg-slate-200 rounded-2xl h-10 outline-none p-3
@@ -41,9 +40,11 @@ const Navbar = () => {
                     </svg>
                 </div>
             </div>
+            </div>
+
 
             <div className="flex items-center">
-                    {/* Account */}
+                {/* Account */}
                 <div className="navbarStart lg:flex">
                     <div>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
@@ -56,8 +57,7 @@ const Navbar = () => {
 
                 {/* Cart */}
                 <div className="flex relative items-center
-                rounded cursor-pointer hover:bg-slate-200 p-2 transition duration-300
-                ">
+                rounded cursor-pointer hover:bg-slate-200 p-2 transition duration-300">
                     <div>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
@@ -83,6 +83,7 @@ const Navbar = () => {
                     </svg>
                 </div>
             </div>
+
         </div>
      );
 }
