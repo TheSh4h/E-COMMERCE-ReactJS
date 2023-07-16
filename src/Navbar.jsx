1fa2d5229f2d08874e38a4df9871878625c4c267
TheSh4h <hh.haseebshah@gmail.com>
2023-07-16 05:41:05 -0700

    fixed responisveness

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     const [showMenu, setShowMenu] = useState(false);
 
     return ( 
-        <div className="flex justify-between items-center m-5
+        <div className="flex justify-between items-center m-5 mt-1
         border-b">
             <div className="flex items-center cursor-pointer">
                 <img src={Kart} alt="Kart" />
@@ -14,32 +14,32 @@ const Navbar = () => {
             </div>
 
             <div className='flex'>
-                <div className="navbarStart"
-                >    
-                    <h1>Deals</h1>
-                </div>
+                    <div className="navbarStart"
+                    >    
+                        <h1>Deals</h1>
+                    </div>
 
-                <div className="navbarStart"
-                >
-                    <h1>What's New</h1>
-                </div>
+                    <div className="navbarStart"
+                    >
+                        <h1>What's New</h1>
+                    </div>
 
-                <div className="navbarStart"
-                >
-                    <h1>Delivery</h1>
-                </div>
+                    <div className="navbarStart"
+                    >
+                        <h1>Delivery</h1>
+                    </div>
 
                 {/* Search field */}
-            <div className="lg:flex hidden items-center relative">
-                <input type="text" placeholder="Search Product" className="
-                bg-slate-200 rounded-2xl h-10 outline-none p-3
-                " />
-                <div className="absolute right-0 p-1 flex items-center cursor-pointer">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
-                        <path fillRule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clipRule="evenodd" />
-                    </svg>
+                <div className="lg:flex hidden items-center relative">
+                    <input type="text" placeholder="Search Product" className="
+                    bg-slate-200 rounded-2xl h-10 outline-none p-3
+                    " />
+                    <div className="absolute right-0 p-1 flex items-center cursor-pointer">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                            <path fillRule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clipRule="evenodd" />
+                        </svg>
+                    </div>
                 </div>
-            </div>
             </div>
 
 
