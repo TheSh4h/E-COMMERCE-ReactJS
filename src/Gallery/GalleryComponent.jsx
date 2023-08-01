1facf60f2d6abe3cece6a8fa4233d05e8e209f07
TheSh4h <hh.haseebshah@gmail.com>
2023-08-01 09:41:34 -0700

    idk what I'm doing at this point

--- src/Gallery/GalleryComponent.jsx
+++ src/Gallery/GalleryComponent.jsx
@@ -1,9 +1,13 @@
-const GalleryComponent = ({img, css, title}) => {
+import GalleryItem from "./GalleryItem";
 
+const GalleryComponent = ({img, css, title}) => {
     return ( 
         <div className='card'>
                 <div>
+                    <a href={<GalleryItem />}>
                     <img src={img} alt="failed to load" className={css}/>
+                    </a>
+                    
                 </div>
                 <div className='badge'>
                     <span className='font-semibold text-white'>{title}</span>
---
+++ src/Gallery/GalleryItem.jsx
@@ -0,0 +1,9 @@
+const GalleryItem = () => {
+    return ( 
+        <>
+        <div className="bg-black">sdkf</div>
+        </>
+     );
+}
+ 
+export default GalleryItem;
