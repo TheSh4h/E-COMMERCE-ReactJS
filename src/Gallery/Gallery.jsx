ae00afac85ddb255dbadbe6a7f66f446a9e3ae22
TheSh4h <hh.haseebshah@gmail.com>
2023-08-11 11:02:57 -0700

    utilized custom hook

--- src/Gallery/Gallery.jsx
+++ src/Gallery/Gallery.jsx
@@ -4,25 +4,25 @@ import gym from '../img/gym.jpg';
 import console from '../img/console.jpg';
 import GalleryComponent from './GalleryComponent';
 import { Link } from 'react-router-dom';
+import useFetch from '../useFetch';
 
 const Gallery = () => {
-    const categories = [
-        { img: gym, title: 'Gym'},
-        { img: books, title: 'Books'},
-        { img: furniture, title: 'Furniture'},
-        { img: console, title: 'Electronics'},
-    ]
+    const { loading, products, error } = useFetch('http://localhost:4000/GalleryItems');
+    
     return ( 
 
     <div className='flex justify-center mb-10'>
 
         <div className='grid lg:grid-cols-4 sm:grid-cols-2 gap-4 m-10'>
 
-            {categories.map((category, index) => (
+            {loading && <p>Loading...</p>}
+            {error && <p>Failed to load resources</p>}
+
+            {products.map((category, index) => (
                 <Link to={`/${category.title}`}>
                     <GalleryComponent
                     key={index}
-                    img={category.img}
+                    img={`../img/${category.img}`}
                     title={category.title}
                     />
                 </Link>
