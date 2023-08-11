ba0a44e47160d2fd893be627dedfeecc6c5e9e9f
TheSh4h <hh.haseebshah@gmail.com>
2023-08-11 11:25:30 -0700

    implemented useParam and useFetch hook

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import RootLayout from './layout/RootLayout';
 import Home from './Home/Home';
+import GalleryItem from './Gallery/GalleryItem';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<RootLayout />}>
       <Route index element={<Home />} />
+      <Route path=':title' element={<GalleryItem />} />
     </Route>
   )
 )
--- src/Gallery/GalleryItem.jsx
+++ src/Gallery/GalleryItem.jsx
@@ -1,10 +1,13 @@
 import { useParams } from "react-router-dom";
+import useFetch from "../useFetch";
 
 const GalleryItem = () => {
-    const { categoryItem } = useParams();
+    const { title } = useParams();
+    const url = 'http://localhost:4000/GalleryItems' + title;
+    const {loading, data, error} = useFetch(url);
     return (
         <div>
-            <h2>{categoryItem}</h2>
+            <h2>{title}</h2>
         </div>
      );
 }
