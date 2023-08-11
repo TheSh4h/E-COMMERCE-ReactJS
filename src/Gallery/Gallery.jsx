5c66d07068fd42ec6645f206e4898176c36e0a28
TheSh4h <hh.haseebshah@gmail.com>
2023-08-11 09:54:20 -0700

    fixed Link

--- src/Gallery/Gallery.jsx
+++ src/Gallery/Gallery.jsx
@@ -3,6 +3,7 @@ import furniture from '../img/furniture.jpg';
 import gym from '../img/gym.jpg';
 import console from '../img/console.jpg';
 import GalleryComponent from './GalleryComponent';
+import { Link } from 'react-router-dom';
 
 const Gallery = () => {
     const categories = [
@@ -18,12 +19,16 @@ const Gallery = () => {
         <div className='grid lg:grid-cols-4 sm:grid-cols-2 gap-4 m-10'>
 
             {categories.map((category, index) => (
-                <GalleryComponent
-                key={index}
-                img={category.img}
-                css={category.css}
-                title={category.title}
-                />
+                <Link to={category.title.toString()}>
+                    {
+                        <GalleryComponent
+                        key={index}
+                        img={category.img}
+                        css={category.css}
+                        title={category.title}
+                        />
+                    }
+                </Link>
             ))}
             
         </div>
--- src/Gallery/GalleryComponent.jsx
+++ src/Gallery/GalleryComponent.jsx
@@ -1,14 +1,9 @@
-import GalleryItem from "./GalleryItem";
-import { Link } from "react-router-dom";
 
 const GalleryComponent = ({img, css, title}) => {
     return ( 
         <div className='card'>
                 <div>
-                    <Link to={`/${title}`}> 
-                        <img src={img} alt="failed to load" className={css}/>
-                    </Link>
-                    
+                    <img src={img} alt="failed to load" className={css}/>                    
                 </div>
                 <div className='badge'>
                     <span className='font-semibold text-white'>{title}</span>
