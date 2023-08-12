e9fcc0b66967bea578cf83734c468edd60d54e45
TheSh4h <hh.haseebshah@gmail.com>
2023-08-12 05:00:04 -0700

    reverted cahnges

--- src/Gallery/Gallery.jsx
+++ src/Gallery/Gallery.jsx
@@ -4,25 +4,25 @@ import gym from '../img/gym.jpg';
 import console from '../img/console.jpg';
 import GalleryComponent from './GalleryComponent';
 import { Link } from 'react-router-dom';
-import useFetch from '../useFetch';
 
 const Gallery = () => {
-    const { loading, data, error } = useFetch('http://localhost:4000/GalleryItems');
-    
+    const categories = [
+        { img: gym, title: 'Gym'},
+        { img: books, title: 'Books'},
+        { img: furniture, title: 'Furniture'},
+        { img: console, title: 'Electronics'},
+    ]
     return ( 
 
     <div className='flex justify-center mb-10'>
 
         <div className='grid lg:grid-cols-4 sm:grid-cols-2 gap-4 m-10'>
 
-            {loading && <p>Loading...</p>}
-            {error && <p>Failed to load resources</p>}
-
-            {data.map((category, index) => (
+            {categories.map((category, index) => (
                 <Link to={`/${category.title}`}>
                     <GalleryComponent
                     key={index}
-                    img={`../img/${category.img}`}
+                    img={category.img}
                     title={category.title}
                     />
                 </Link>
