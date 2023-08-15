2d30e235b0e92a1ae00d60457c6a49255ed5918b
TheSh4h <hh.haseebshah@gmail.com>
2023-08-15 11:22:32 -0700

    disabled decrement button

--- src/pages/ItemList.jsx
+++ src/pages/ItemList.jsx
@@ -47,7 +47,7 @@ const ItemList = () => {
                     {/* counter */}
                     <div className="flex items-center justify-evenly
                     bg-slate-200 rounded-full h-[60px] w-[180px]">
-                        <button onClick={decrement}>
+                        <button disabled={count === 1} onClick={decrement}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12h-15" />
                             </svg>
