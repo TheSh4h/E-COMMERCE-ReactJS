d21ac0b920ee5039d6c7967f8ccbe3d196399211
TheSh4h <hh.haseebshah@gmail.com>
2023-09-01 22:25:48 -0700

    color property fixed in cart

--- src/Cart/CartItems.jsx
+++ src/Cart/CartItems.jsx
@@ -67,8 +67,8 @@ const CartItems = ({ id, image, title, price, quantity }) => {
                     </button>
                 </div>
                 <button onClick={() => dispatch({ type: 'REMOVE_FROM_CART', payload: { id: id, title: title } })}
-                className="bg-black text-white font-semibold p-1 rounded-lg mt-2
-                hover:bg-slate-600 transition delay-75 px-5 py-1">
+                className="bg-green-950 text-white font-semibold p-1 rounded-lg mt-2
+                hover:bg-white hover:text-black border border-green-950 transition delay-75 px-5 py-1">
                     Delete Item
                 </button>
             </div>
