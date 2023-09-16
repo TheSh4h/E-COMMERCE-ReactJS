c077b77e82062416e95384560519807c6bcf3a5f
TheSh4h <hh.haseebshah@gmail.com>
2023-09-16 11:11:32 -0700

    towards pagintion

--- src/pages/Category Pages/Outfit.jsx
+++ src/pages/Category Pages/Outfit.jsx
@@ -1,9 +1,18 @@
 import { Link } from "react-router-dom";
 import TopPicksItem from "../../TopPicks/TopPicksItem";
 import useFetch from "../../useFetch";
+import ReactPaginate from "react-paginate";
+import { useState } from "react";
 
 const Outfit = () => {
     const { loading, data, error }  = useFetch('http://localhost:4000/outfits')
+    const [items, setItems] = useState(data.slice(0, data.length));
+    console.log(items)
+    const [pageNumber, setPageNumber] = useState(0);
+
+    const itemsPerPage = 10;
+    const pagesVisited = pageNumber * itemsPerPage
+
     return ( 
         <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-8 m-5">
             {loading && <p>Loading...</p>}
