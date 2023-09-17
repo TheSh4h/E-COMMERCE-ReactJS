902bf20e9be6402daa63e16e720e5c63085ddc2b
TheSh4h <hh.haseebshah@gmail.com>
2023-09-17 10:19:55 -0700

    successfully paginated

--- src/pages/Category Pages/Outfit.jsx
+++ src/pages/Category Pages/Outfit.jsx
@@ -1,35 +1,65 @@
+import "./paginate.css"
 import { Link } from "react-router-dom";
 import TopPicksItem from "../../TopPicks/TopPicksItem";
 import useFetch from "../../useFetch";
 import ReactPaginate from "react-paginate";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Outfit = () => {
     const { loading, data, error }  = useFetch('http://localhost:4000/outfits')
-    const [items, setItems] = useState(data.slice(0, data.length));
-    console.log(items)
+    const [items, setItems] = useState([]);
     const [pageNumber, setPageNumber] = useState(0);
+    const itemsPerPage = 9;
 
-    const itemsPerPage = 10;
-    const pagesVisited = pageNumber * itemsPerPage
+    useEffect(() => {
+        window.scrollTo({top: 0, behavior: "smooth"})
+    }, [pageNumber]);
 
+    useEffect(() => {
+        if(!loading) {
+            setItems(data.slice(pageNumber * itemsPerPage, (pageNumber + 1) * itemsPerPage))
+        }
+    }, [loading, data, pageNumber]);
+
+    const pageCount = Math.ceil(data.length / itemsPerPage);
+
+    const changePage = ({selected}) => {
+        setPageNumber(selected)
+    }
+    
     return ( 
-        <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-8 m-5">
-            {loading && <p>Loading...</p>}
-            {error && <p>Failed to load resources</p>}
+        <div>
+                {loading && <p>Loading...</p>}
+                {error && <p>Failed to load resources</p>}
+                
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-8 m-5">
+                {items.map((product, index) => (
+                    <Link to={`/Men's Outfit/${product.id}`} key={index}>
+                        <TopPicksItem
+                            key={index}
+                            image={`../img/${product.image}`}
+                            title={product.title}
+                            description={product.description}
+                            price={product.price} 
+                            tag={product.tag}
+                        />
+                    </Link>
+                ))}
 
-            {data.map((product, index) => (
-                <Link to={`/Men's Outfit/${product.id}`} key={index}>
-                    <TopPicksItem
-                        key={index}
-                        image={`../img/${product.image}`}
-                        title={product.title}
-                        description={product.description}
-                        price={product.price} 
-                        tag={product.tag}
+            </div>
+                <div className="paginate">
+                    <ReactPaginate 
+                    previousLabel={"<<"}
+                    nextLabel={">>"}
+                    pageCount={pageCount}
+                    onPageChange={changePage}
+                    containerClassName={"paginationBttns"}
+                    previousLinkClassName={"previousBttn"}
+                    nextLinkClassName={"nextBttn"}
+                    disabledClassName={"paginationDisabled"}
+                    activeClassName={"paginationActive"}
                     />
-                </Link>
-            ))}
+                </div>
         </div>
      );
 }
