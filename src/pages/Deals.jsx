0648834b0d315cc5baabd05229a9de73cd9f9274
TheSh4h <hh.haseebshah@gmail.com>
2023-11-07 21:12:50 +0500

    deals done

--- src/pages/Deals.jsx
+++ src/pages/Deals.jsx
@@ -1,7 +1,13 @@
 const Deals = () => {
     return ( 
-        <div>
-            Sorry there are not not any deals available in your region
+        <div className="rounded m-1  h-52 lg:h-96 text-center comtainer mx-auto">
+            <div className="flex justify-center items-center h-full">
+                <div>
+                    <h1 className="font-semibold text-2xl text-green-900">Sorry for inconvenience</h1>
+                    <p>There aren't any deals available in your region</p>
+                    <p>You already know that I can do the job. So piss off</p>
+                </div>
+            </div>
         </div>
      );
 }
